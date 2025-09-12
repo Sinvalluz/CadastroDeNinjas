fdef13df21666f523eeb8cd11159c09d6577042c
Sinval Luz <contato.sinvalluz@gmail.com>
2025-09-12 08:44:10 -0300

    Criado a controller

---
+++ src/main/java/dev/sinval/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.sinval.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVinda () {
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+}
